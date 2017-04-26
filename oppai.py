9f392ab07aa00fe2cff8b6e56928c82aaee5ca61
beiz23 <beiz23@mstdn-workers>
2017-04-26 23:49:17 +0900

    Add new file: oppai.py

---
+++ oppai.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fileencoding=utf-8
+
+class Oppai:
+    """A oppai massage class"""
+    def touch(self):
+        return "やん"
+
+pai = Oppai()
+print pai.touch()
+
+
+
