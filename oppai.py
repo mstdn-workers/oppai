c017fe4e83a4afd8c2917bebee77ca572cf6d4ad
beiz23 <contact@beiznotes.org>
2017-04-27 20:38:30 +0900

    [add] add nurse function to oppai.py
    for my exercise

--- oppai.py
+++ oppai.py
@@ -3,12 +3,35 @@
 # vim:fileencoding=utf-8
 
 class Oppai:
-    """A oppai massage class"""
+    """An oppai nurse class"""
+    def __init__(self):
+        self._milk = 0
+
+    def __init__(self, milk):
+        self._milk = milk
+
     def touch(self):
         return "やん"
 
-pai = Oppai()
+    def nurse(self, milk):
+        self._milk -= milk
+
+    def showVolOfMilk(self):
+        print "milk: ", self._milk, " ml"
+        print
+
+
+print "Make 100 ml oppai"
+pai = Oppai(100)
+pai.showVolOfMilk()
+
+print "Nurse 45 ml"
+pai.nurse(45)
+pai.showVolOfMilk()
+
+print "pai touch"
 print pai.touch()
+
 
 
 
