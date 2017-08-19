aadca33505c90e8b1c20786264716ed7cbd8c62b
mochimilk <31152781+mochimilk@users.noreply.github.com>
2017-08-20 04:26:30 +0900

    Update pypy.py

--- pypy.py
+++ pypy.py
@@ -1,10 +1,30 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import oppai
+#import oppai
+class Oppai:
+    """An oppai nurse class"""
+    def __init__(self):
+        self._milk = 0
+
+    def __init__(self, milk):
+        self._milk = milk
+
+    def touch(self):
+        return "やん"
+
+    def nurse(self, milk):
+        self._milk -= milk
+
+    def showVolOfMilk(self):
+        print "milk: ", self._milk, " ml"
+        print
 
 yun = Oppai(50)
 
 yun.showVolOfMilk()
+
+print "<html>\n<body>\n"
 print "YUN 「"
 print yun.touch()
+print "\n</body>\n</html>"
