79af10b92cbca42cb83033087a83b73024c7cfa2
mochimilk <31152781+mochimilk@users.noreply.github.com>
2017-08-20 04:43:41 +0900

    Update and rename prpr.txt to pypy.py

---
+++ pypy.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import oppai
+
+yun = Oppai(50)
+
+yun.showVolOfMilk()
+
+print ("<html>\n")
+print ("<title>",yun.showVolOfMilk(),"</title>\n")
+print ("<body>\n")
+print ("YUN 「")
+print (yun.touch())
+print ("\n</body>\n</html>")
